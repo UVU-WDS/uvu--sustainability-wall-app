cb0a45037935024f6a4a1d61ab2661a79761cebc
Kameron Black <10733374@AD.UVU.EDU>
2018-06-01 14:18:24 -0600

    Started line animation

--- actions.js
+++ actions.js
@@ -1,5 +1,13 @@
 $(function(){
 
+$('.svg-border').each(function(){
+    var length = this.getTotalLength();
+    var dynamiclength = this.getTotalLength() - (Math.floor(Math.random() * Math.floor(this.getTotalLength())) / 2);
+    $(this)
+        .css('stroke-dasharray', length)
+        .css('stroke-dashoffset', length);
+});
+
 $('nav li').on('click', function(){
     $('nav .active').removeClass('active');
     $(this).addClass('active');
