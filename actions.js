1e1f17df207244276f40cad318f2b397e47f5081
kamblack <kamboblack@gmail.com>
2018-06-06 12:24:33 -0600

    Design draft, first tab

--- actions.js
+++ actions.js
@@ -8,12 +8,44 @@ $('.svg-border').each(function(){
         .css('stroke-dashoffset', length);
 });
 
+var somethingOpen = false;
+
+function cleanScreen(){
+    if (somethingOpen) {
+        $('section.open svg, section.open .summary, section.open .icon.open').addClass('hiding');
+    } else {
+        $('.hiding').removeClass('hiding');
+    }
+}
+
+function closeAll(){
+    $('section > div.opened').removeClass('opened');
+    somethingOpen = false;
+    cleanScreen();
+}
+
 $('nav li').on('click', function(){
+    closeAll();
     $('nav .active').removeClass('active');
     $(this).addClass('active');
     var id = $(this).attr('id');
     $('main section').removeClass('open');
     $('#section-'+id).addClass('open');
 });
+
+$('.icon.closed').on('click', function(e){
+    var id = $(this).attr('data-opens');
+    if ($('#'+id).hasClass('opened')) {
+        $('#'+id).removeClass('opened');
+        somethingOpen = false;
+    } else {
+        closeAll();
+        $('#'+id).addClass('opened');
+        somethingOpen = true;
+    }
+    cleanScreen();
+});
+
+$('.close-icon').on('click', closeAll);
 
 });
